9191e6821045b64c823053f1997296bebae74a48
FeeAlan <fee752115651@gmail.com>
2018-06-08 10:03:24 +0800

    完成反转数

---
+++ reversal_number/reversal_number.py
@@ -0,0 +1,48 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import math
+
+"""
+给定一个 32 位有符号整数，将整数中的数字进行反转。
+
+示例 1:
+
+输入: 123
+输出: 321
+ 示例 2:
+
+输入: -123
+输出: -321
+示例 3:
+
+输入: 120
+输出: 21
+注意:
+
+假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−2^31,  2^31 − 1]。根据这个假设，如果反转后的整数溢出，则返回 0。
+"""
+
+
+def reverse(number):
+    max = math.pow(2, 31) - 1
+    min = -math.pow(2, 31)
+    y = 0
+    temp = number
+    temp = abs(temp)
+    while temp != 0:
+        x = int(temp % 10)
+        y = y * 10 + x
+        if y > max or y < min:
+            return 0
+        temp = int(temp / 10)
+    if number > 0:
+        return y
+    else:
+        return -y
+
+
+if __name__ == "__main__":
+    num = 1534236469
+    result = 0
+    reverse_num = reverse(num)
+    print(reverse_num == result)
